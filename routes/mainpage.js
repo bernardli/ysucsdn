dfb885a602e51649ff61c8e9ac56f910b55f164b
libo <623807914@qq.com>
2017-02-14 00:53:54 +0800

    feat:优化首页CSS

--- routes/mainpage.js
+++ routes/mainpage.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var PostModel = require('../models/posts');
+
 //获取解析后的主页页面
 router.get('/', function(req, res, next) {
-    res.render('mainpage');
+    PostModel.getPosts()
+        .then(function(posts) {
+            res.render('mainpage', {
+                posts: posts
+            });
+        })
+        .catch(next);
 });
 
 module.exports = router;
