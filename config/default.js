1eca5589dc3ea63c84f627f0155d35d7383cf3f4
puresox <13004384112@163.com>
2017-02-15 11:16:16 +0800

    规范

--- config/default.js
+++ config/default.js
@@ -1,9 +1,9 @@
 module.exports = {
     port: 3000,
     session: {
-        secret: 'myblog',
-        key: 'myblog',
+        secret: 'blog',
+        key: 'blog',
         maxAge: 2592000000
     },
-    mongodb: 'mongodb://localhost:27017/myblog'
+    mongodb: 'mongodb://admin:admin@localhost:27017/blog'
 };
