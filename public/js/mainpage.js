929d69cb731c6ff5564f45568ec7e82ca2316ec2
BernardLi <bernardli1248@qq.com>
2017-02-26 22:56:02 +0800

    feat:首页新增照片展示和点击浏览

---
+++ public/js/mainpage.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.pic-show-box').css({
+        'width': $(window).width(),
+        'height': $(window).height()
+    });
+
+    $('.pic-show-box-filter').css({
+        'width': $(window).width(),
+        'height': $(window).height()
+    });
+
+    $('.pic-item').click(function(e) {
+        console.log('show');
+        $('.pic-show-box-filter').toggleClass('hide');
+        $('.pic-show-box').toggleClass('hide');
+        $('.img-show-detail').attr('src', $(e.target).attr('src'));
+    });
+
+    $('.pic-show-box-header').click(function() {
+        console.log('hide');
+        $('.pic-show-box').toggleClass('hide');
+        $('.pic-show-box-filter').toggleClass('hide');
+    });
+
+    console.log('ready');
+});
