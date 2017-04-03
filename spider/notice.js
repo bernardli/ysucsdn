938c9c8fb0a59ab976722818dd07ea7dc38570da
puresox <13004384112@163.com>
2017-04-03 14:42:36 +0800

    修改邮箱通知文本

--- spider/notice.js
+++ spider/notice.js
@@ -22,11 +22,11 @@ exports.spiderNotice = () => {
           from: EmailAdress, // 发件人
           to: adminEmail, // 收件人
           subject: 'ysunotice', // 标题
-          text: `有新通知啦，${time}发表了《${title}》`, // 内容
-          html: '', // html
+          text: '', // 内容
+          html: `<p>有新通知啦，${time}发表了《${title}》</p><a href='http://notice.ysu.edu.cn${href}'>http://notice.ysu.edu.cn${href}</a>`, // html
         };
         EmailModel.email(mailOptions);
-        console.log(`有新通知啦，${time}发表了《${title}》`);
+        console.log(`有新通知啦，${time}发表了《${title}》http://notice.ysu.edu.cn${href}`);
         newNotice(notice, $, i + 1);
       }
     })
@@ -46,17 +46,29 @@ exports.spiderNotice = () => {
         NoticeModel.oneHtml('http://notice.ysu.edu.cn/')
           .then(($) => {
             const href = $('li a').eq(0).attr('href');
-            console.log(`http://notice.ysu.edu.cn${href}`);
-            return NoticeModel.oneETag(`http://notice.ysu.edu.cn${href}`);
+            return Promise.all([
+              NoticeModel.oneETag(`http://notice.ysu.edu.cn${href}`),
+              $,
+            ]);
           })
-          .then((firstETag) => {
+          .then(([firstETag, $]) => {
+            const href = $('li a').eq(0).attr('href');
+            const time = $('li a span').eq(0).text();
+            const title = $('li a').eq(0).attr('title');
             const Notice = {
               name: 'notice',
               firstETag,
             };
             NoticeModel.create(Notice);
-            NoticeModel.sendMeRes('', '数据库初始化成功');
-            console.log('数据库初始化成功');
+            const mailOptions = {
+              from: EmailAdress, // 发件人
+              to: adminEmail, // 收件人
+              subject: 'ysunotice', // 标题
+              text: '', // 内容
+              html: `<p>数据库初始化成功，最新文章为${time}发表的《${title}》</p><a href='http://notice.ysu.edu.cn${href}'>http://notice.ysu.edu.cn${href}</a>`, // html
+            };
+            EmailModel.email(mailOptions);
+            console.log(`数据库初始化成功，最新文章为${time}发表的《${title}》 http://notice.ysu.edu.cn${href}`);
           })
           .catch((err) => {
             NoticeModel.sendMeRes(err, '数据库初始化失败：');
