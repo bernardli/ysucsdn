38527440268b63f68297f8685c901eea36f61959
puresox <13004384112@163.com>
2017-04-05 11:39:21 +0800

    feat(): 每天早晚发送状态报告，顺便解决了时间格式问题

--- spider/notice.js
+++ spider/notice.js
@@ -2,11 +2,13 @@ const config = require('config-lite');
 const EmailNoticeModel = require('../models/emailNotice');
 const EmailModel = require('../models/sendEmail');
 const NoticeModel = require('../models/ysuNotice');
+const moment = require('moment');
 
 const EmailAdress = config.transporter.auth.user;
 const adminEmail = config.adminEmail;
 
 exports.spiderNotice = () => {
+  // 递归发消息
   function newNotice(notice, $$, i) {
     const href = $$('li a').eq(i).attr('href');
     Promise.all([
@@ -21,6 +23,7 @@ exports.spiderNotice = () => {
           console.log('系统侦测到严重漏洞，已启动自毁程序');
         } else if (notice.firstETag !== header.etag) {
           let { 'last-modified': time } = header;          // 必须是let
+          time = moment(time).format('YYYY-MM-DD，H:mm:ss');
           const title = $('td .titlestyle50830').text().trim();
           const query = {
             ysuNotice: 'y',
@@ -55,9 +58,24 @@ exports.spiderNotice = () => {
       });
   }
 
+  // 早晚报告情况
+  function stateNotice() {
+    const morningB = moment('7:00:00', 'H:mm:ss');
+    const morningE = moment('7:10:00', 'H:mm:ss');
+    const nightB = moment('22:00:00', 'H:mm:ss');
+    const nightE = moment('22:10:00', 'H:mm:ss');
+    const timeNow = moment(new Date(), 'H:mm:ss');
+    if (timeNow > morningB && timeNow < morningE) {
+      NoticeModel.sendMeRes('大佬早上好哟，通知系统今天也是元气满满的呢', '');
+    } else if (timeNow > nightB && timeNow < nightE) {
+      NoticeModel.sendMeRes('大佬晚安啦，通知系统晚上也是元气满满的呢', '');
+    }
+  }
+
   console.log('通知监控系统启动成功');
 
   function notice() {
+    stateNotice();
     NoticeModel.getNotice('notice')
       .then((result) => {
         const notice = result[0];
@@ -85,6 +103,7 @@ exports.spiderNotice = () => {
             })
             .then(([$, header, href]) => {
               let { 'last-modified': time } = header;         // 必须是let
+              time = moment(time).format('YYYY-MM-DD，H:mm:ss');
               const title = $('td .titlestyle50830').text().trim();
 
               const mailOptions = {
