13b1fe1fbef4dbc1072f0a4fbca966d17981f371
puresox <13004384112@163.com>
2017-04-14 17:30:08 +0800

    fix(获取标题的bug): 获取标题的bug
    Closes #65

--- models/noticeFunction.js
+++ models/noticeFunction.js
@@ -1,26 +1,39 @@
 const NoticeModel = require('../models/ysuNotice');
+const moment = require('moment');
 
 module.exports = {
   updateNoticeByHtml: function updateNoticeByHtml() {
     NoticeModel.requestOne('http://notice.ysu.edu.cn/')
       .then(([$]) => {
-        const href = $('a', '#lineu12_0').attr('href');
+        let href = $('a', '#lineu12_0').attr('href');
+        const title = $('.list-txt-1', '#lineu12_0').text().trim();
         const patt = new RegExp('http://');
-        if (patt.test(href)) {
-          return NoticeModel.requestOne(href);
+        if (!patt.test(href)) {
+          href = `http://notice.ysu.edu.cn/${href}`;
         }
-        return NoticeModel.requestOne(`http://notice.ysu.edu.cn/${href}`);
+        return Promise.all([
+          NoticeModel.requestOne(href),
+          title,
+        ]);
       })
-      .then(([$, header]) => {
-        let title = $('h3', '.content-title').text().trim();
+      .then(([
+        [, {
+          'last-modified': time,
+          etag,
+        }], title,
+      ]) => {
+        if (!etag) {
+          etag = moment().format();
+        }
+        time = moment(time).format('YYYY-MM-DD，H:mm:ss');
         if (!title) {
           title = '权限不足，无法访问';
         }
         const data = {
-          firstETag: header.etag,
+          firstETag: etag,
         };
         NoticeModel.updateNoticeByName('notice', data);
-        NoticeModel.sendMeRes(`<p>发生错误，已自动更新数据库为最新记录《${title}》。错误可能有：1.可能有通知被删除。2.可能短时间发表大量文章。3.连接超时。4.其他异常。</p>`);
+        NoticeModel.sendMeRes(`<p>发生错误，已自动更新数据库为最新记录：${time}发表的《${title}》。错误可能有：1.可能有通知被删除。2.可能短时间发表大量文章。3.连接超时。4.其他异常。</p>`);
       })
       .catch((err) => {
         NoticeModel.sendMeRes(`<p>${err}</p>`);
--- spider/notice.js
+++ spider/notice.js
@@ -9,9 +9,9 @@ const EmailAdress = config.transporter.auth.user;
 
 exports.spiderNotice = () => {
   // 递归发消息
-  function newNotice(notice, $$, i) {
+  const newNotice = (notice, $$, i) => {
     Promise.all([
-      $$('a',`#lineu12_${i}`).attr('href'),
+      $$('a', `#lineu12_${i}`).attr('href'),
     ])
       .then(([href]) => {
         const patt = new RegExp('http://');
@@ -24,20 +24,23 @@ exports.spiderNotice = () => {
         NoticeModel.requestOne(href),
         href,
       ]))
-      .then(([[$, header], href]) => {
-        if (!(header && $ && href)) {
-          throw new Error('网站访问出错');
-        } else if (i === 6) {
+      .then(([
+        [, {
+          'last-modified': time,
+          etag,
+        }], href,
+      ]) => {
+        if (!etag) {
+          etag = moment().format();
+        }
+        if (i === 6) {
           NoticeFuncModel.updateNoticeByHtml();
           stopNotice();
-        } else if (notice.firstETag !== header.etag) {
-          let {
-            'last-modified': time,
-          } = header; // 必须是let
+        } else if (notice.firstETag !== etag) {
           time = moment(time).format('YYYY-MM-DD，H:mm:ss');
-          let title = $('h3','.content-title').text().trim();
+          let title = $$('.list-txt-1', `#lineu12_${i}`).text().trim();
           if (!title) {
-            title = '权限不足，无法访问';
+            title = '获取标题出错';
           }
           EmailNoticeModel.getNotice({
             ysuNotice: 'y',
@@ -49,23 +52,23 @@ exports.spiderNotice = () => {
                     name: 'YSUCSDN',
                     address: EmailAdress,
                   }, // 发件人
-                  to: user.user.email, // 收件人
+                  to: [user.user.email], // 收件人
                   subject: '燕山大学通知监控服务发现新通知!', // 标题
                   text: '', // 内容
                   html: `<p>通知监控系统发现新通知！</p><p>${time}发表了《${title}》</p><p>如果你对这个通知不感兴趣，请无视，如果感兴趣，请点击下方链接：</p><a href='${href}'>${href}</a>`, // html
                 };
                 EmailModel.email(mailOptions);
               });
-              console.log(`通知监控系统发现新通知，${time}发表了《${title}》${href}`);
+              // console.log(`通知监控系统发现新通知，${time}发表了《${title}》${href}`);
               newNotice(notice, $$, i + 1);
             })
             .catch((err) => {
               NoticeModel.sendMeRes(`<p>${err}</p>`);
-              console.log(err);
+              // console.log(err);
             });
           if (i === 0) {
             const data = {
-              firstETag: header.etag,
+              firstETag: etag,
             };
             NoticeModel.updateNoticeByName('notice', data);
           }
@@ -73,14 +76,14 @@ exports.spiderNotice = () => {
       })
       .catch((err) => {
         NoticeModel.sendMeRes(`<p>${err}</p>`);
-        console.log(err);
+        // console.log(err);
       });
-  }
+  };
 
   // 防止通知被删除导致逻辑错误，先检索一遍
-  function preNotice(notice, $$, i) {
+  const preNotice = (notice, $$, i) => {
     Promise.all([
-      $$('a',`#lineu12_${i}`).attr('href'),
+      $$('a', `#lineu12_${i}`).attr('href'),
     ])
       .then(([href]) => {
         const patt = new RegExp('http://');
@@ -90,56 +93,65 @@ exports.spiderNotice = () => {
         return `http://notice.ysu.edu.cn/${href}`;
       })
       .then(href => NoticeModel.requestOne(href))
-      .then(([$, header]) => {
-        if (!(header && $)) {
-          throw new Error('网站访问出错');
-        } else if (i === 6) {
+      .then(([, {
+        etag,
+      }]) => {
+        if (!etag) {
+          etag = moment().format();
+        }
+        if (i === 6) {
           NoticeFuncModel.updateNoticeByHtml();
-        } else if (notice.firstETag !== header.etag) {
+        } else if (notice.firstETag !== etag) {
           preNotice(notice, $$, i + 1);
-        } else if (notice.firstETag === header.etag && i !== 0) {
+        } else if (notice.firstETag === etag && i !== 0) {
           newNotice(notice, $$, 0);
         }
       })
       .catch((err) => {
         NoticeModel.sendMeRes(`<p>${err}</p>`);
-        console.log(err);
+        // console.log(err);
       });
-  }
+  };
 
-  console.log('通知监控系统启动成功');
+  // console.log('通知监控系统启动成功');
 
-  function Notice() {
-    console.log('通知监控系统运行正常');
+  const Notice = () => {
+    // console.log('通知监控系统运行正常');
     NoticeModel.getNotice('notice')
       .then((result) => {
         const notice = result[0];
         if (!notice) {
           NoticeModel.requestOne('http://notice.ysu.edu.cn/')
             .then(([$]) => {
-              const href = $('a','#lineu12_0').attr('href');
+              let href = $('a', '#lineu12_0').attr('href');
+              const title = $('.list-txt-1', '#lineu12_0').text().trim();
               const patt = new RegExp('http://');
-              if (patt.test(href)) {
-                return href;
+              if (!patt.test(href)) {
+                href = `http://notice.ysu.edu.cn/${href}`;
               }
-              return `http://notice.ysu.edu.cn/${href}`;
+              return [href, title];
             })
-            .then(href => Promise.all([
+            .then(([href, title]) => Promise.all([
               NoticeModel.requestOne(href),
               href,
+              title,
             ]))
-            .then(([[$, header], href]) => {
-              let {
+            .then(([
+              [, {
                 'last-modified': time,
-              } = header; // 必须是let
+                etag,
+              }], href, title,
+            ]) => {
+              if (!etag) {
+                etag = moment().format();
+              }
               time = moment(time).format('YYYY-MM-DD，H:mm:ss');
               const data = {
                 name: 'notice',
-                firstETag: header.etag,
+                firstETag: etag,
               };
-              let title = $('h3','.content-title').text().trim();
               if (!title) {
-                title = '权限不足，无法访问';
+                title = '获取标题出错';
               }
               return Promise.all([
                 href,
@@ -150,11 +162,11 @@ exports.spiderNotice = () => {
             })
             .then(([href, time, title]) => {
               NoticeModel.sendMeRes(`<p>数据库初始化成功，最新文章为 ${time} 发表的《${title}》</p><a href='${href}'>${href}</a>`);
-              console.log(`数据库初始化成功，最新文章为${time}发表的《${title}》 ${href}`);
+              // console.log(`数据库初始化成功，最新文章为${time}发表的《${title}》 ${href}`);
             })
             .catch((err) => {
               NoticeModel.sendMeRes(`<p>数据库初始化失败：<p>${err}</p></p>`);
-              console.log(err);
+              // console.log(err);
             });
         } else {
           NoticeModel.requestOne('http://notice.ysu.edu.cn/')
@@ -163,23 +175,23 @@ exports.spiderNotice = () => {
             })
             .catch((err) => {
               NoticeModel.sendMeRes(`<p>${err}</p>`);
-              console.log(err);
+              // console.log(err);
             });
         }
       })
       .catch((err) => {
         NoticeModel.sendMeRes(`<p>${err}</p>`);
-        console.log(err);
+        // console.log(err);
       });
-  }
+  };
 
-  // Notice();                    // 测试用
+  // Notice(); // 测试用
 
   const flag = setInterval(Notice, 600000);
 
-  function stopNotice() {
+  const stopNotice = () => {
     clearInterval(flag);
     NoticeModel.sendMeRes('<p>系统侦测到严重漏洞，已启动自毁程序</p>');
-    console.log('系统侦测到严重漏洞，已启动自毁程序');
-  }
+    // console.log('系统侦测到严重漏洞，已启动自毁程序');
+  };
 };
